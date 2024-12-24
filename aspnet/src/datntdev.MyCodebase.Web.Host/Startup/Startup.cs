988c72c333d1af7ba9d0259a653b97a966c0f66e
Dat Nguyen <datntdev@outlook.com>
2024-12-24 23:37:41 +0700

    build: update configuration of logging

--- aspnet/src/datntdev.MyCodebase.Web.Host/Startup/Startup.cs
+++ aspnet/src/datntdev.MyCodebase.Web.Host/Startup/Startup.cs
@@ -73,10 +73,7 @@ namespace datntdev.MyCodebase.Web.Host.Startup
             services.AddAbpWithoutCreatingServiceProvider<MyCodebaseWebHostModule>(
                 // Configure Log4Net logging
                 options => options.IocManager.IocContainer.AddFacility<LoggingFacility>(
-                    f => f.UseAbpLog4Net().WithConfig(_hostingEnvironment.IsDevelopment()
-                        ? "log4net.config"
-                        : "log4net.Production.config"
-                    )
+                    f => f.UseAbpLog4Net().WithConfig("log4net.config")
                 )
             );
         }
