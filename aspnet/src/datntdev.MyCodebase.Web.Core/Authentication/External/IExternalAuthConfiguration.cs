e8895c542cd854c3b8d65984d71249860b50b23d
Dat Nguyen <datntdev@outlook.com>
2024-12-31 16:00:58 +0700

    refactor: remove external authentication

--- aspnet/src/datntdev.MyCodebase.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace datntdev.MyCodebase.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
