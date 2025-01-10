b08d8f23dc5487a378839e516df31a4acba74a3a
Dat Nguyen <datntdev@outlook.com>
2025-01-11 02:09:41 +0700

    refactor: use spinner instead of freeze-ui

--- angular/src/assets/abp-web-resources/abp.freeze-ui.js
+++ angular/src/assets/abp-web-resources/abp.freeze-ui.js
@@ -3,24 +3,12 @@
 var abp = abp || {};
 
 (function () {
-  if (!FreezeUI || !UnFreezeUI) {
-    return;
-  }
 
-  abp.ui.setBusy = function (elm, text, delay) {
-    console.log({elm, text, delay});
-    // FreezeUI({
-    //   element: elm,
-    //   text: text ? text : " ",
-    //   delay: delay
-    // });
+  abp.ui.setBusy = function () {
+    document.querySelector("#main-spinner").classList.remove("d-none");
   };
 
-  abp.ui.clearBusy = function (elm, delay) {
-    console.log({elm, delay});
-    // UnFreezeUI({
-    //   element: elm,
-    //   delay: delay
-    // });
+  abp.ui.clearBusy = function () {
+    document.querySelector("#main-spinner").classList.add("d-none");
   };
 })();
