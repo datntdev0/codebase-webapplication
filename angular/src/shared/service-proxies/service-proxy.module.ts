f14b675feee3ef2ce84b4f82f88823ad6d1ed87f
Dat Nguyen <datntdev@outlook.com>
2025-01-19 17:41:07 +0700

    fixbug: duplicated error rising

--- angular/src/shared/service-proxies/service-proxy.module.ts
+++ angular/src/shared/service-proxies/service-proxy.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AbpHttpInterceptor } from 'abp-ng2-module';
 
 import * as ApiServiceProxies from './service-proxies';
 
@@ -11,7 +9,6 @@ import * as ApiServiceProxies from './service-proxies';
         ApiServiceProxies.SessionServiceProxy,
         ApiServiceProxies.TenantsServiceProxy,
         ApiServiceProxies.UsersServiceProxy,
-        { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true }
     ]
 })
 export class ServiceProxyModule { }
